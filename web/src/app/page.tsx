b057b4e506e401dbbb5ebf4075ab0913e700f572
temitopeohassan <temitope.hassan.2@gmail.com>
2024-07-18 17:08:03 +0100

    starter files commit

---
+++ web/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import React, { useEffect } from 'react';
+import  Header  from './Header'
+
+export default function Home() {
+ 
+
+
+  return (
+    <>
+      <Header />
+      
+    </>
+  );
+}
